06da188db216295ff141b5a518695ea3c21295b2
Kaustav <kstvds61@gmail.com>
2025-12-16 21:05:36 +0530

    Completed introducing Token Caching in the project to improve time of the framework

--- src/test/java/com/api/utils/AuthTokenProvider.java
+++ src/test/java/com/api/utils/AuthTokenProvider.java
@@ -4,42 +4,43 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.api.constants.Role;
 import com.api.request.model.UserCredentials;
 
 import io.restassured.http.ContentType;
 
 public class AuthTokenProvider {
-	
-	private AuthTokenProvider()
-	{
-		
+	private static Map<Role, String> tokenCache = new ConcurrentHashMap<Role, String>();
+
+	private AuthTokenProvider() {
+
 	}
 
 	public static String getToken(Role role) {
-	
-		//String role = "fd";
-	 UserCredentials user = null;
-	 if(role == Role.FD)
-	 {
-		 user = new UserCredentials("iamfd", "password");
-	 }
-	 else if(role == Role.SUP)
-	 {
-		 user = new UserCredentials("iamsup", "password");
-	 }
-	 else if(role == Role.ENG)
-	 {
-		 user = new UserCredentials("iameng", "password");
-	 }
-	 else if(role == Role.QC)
-	 {
-		 user = new UserCredentials("iamqc", "password");
-	 }
-	 String token = given().baseUri(ConfigManager.getProperty("BASE_URI")).contentType(ContentType.JSON).body(user).log().uri().log().body().log().headers().post("login").then()
-	 .log().all().statusCode(200).body("message", equalTo("Success")).body("data.token", notNullValue()).extract().jsonPath().getString("data.token");
-	System.out.println(token);
-	 return token;
+		if (tokenCache.containsKey(role)) {
+			return tokenCache.get(role);
+		}
+
+		UserCredentials user = null;
+		if (role == Role.FD) {
+			user = new UserCredentials("iamfd", "password");
+		} else if (role == Role.SUP) {
+			user = new UserCredentials("iamsup", "password");
+		} else if (role == Role.ENG) {
+			user = new UserCredentials("iameng", "password");
+		} else if (role == Role.QC) {
+			user = new UserCredentials("iamqc", "password");
+		}
+		String token = given().baseUri(ConfigManager.getProperty("BASE_URI")).contentType(ContentType.JSON).body(user)
+				.log().uri().log().body().log().headers().post("login").then().log().all().statusCode(200)
+				.body("message", equalTo("Success")).body("data.token", notNullValue()).extract().jsonPath()
+				.getString("data.token");
+		System.out.println(token);
+		tokenCache.put(role, token);
+		return token;
 	}
 
 }
