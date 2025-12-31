6bf710ca02af0bb6a874b4756061c33652d4230a
Kaustav <kstvds61@gmail.com>
2025-12-31 11:59:41 +0530

    Implemented logger for all servie classes

--- src/test/java/com/api/services/UserDetailsService.java
+++ src/test/java/com/api/services/UserDetailsService.java
@@ -3,6 +3,9 @@ package com.api.services;
 import static com.api.utils.SpecUtil.RequestSpecWithAuth;
 import static io.restassured.RestAssured.given;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.api.constants.Role;
 
 import io.restassured.response.Response;
@@ -14,8 +17,10 @@ public class UserDetailsService {
 	}
 
 	private static final String USER_DETAILS_ENDPOINT = "userdetails";
+	private static final Logger LOGGER = LogManager.getLogger(UserDetailsService.class);
 
 	public static Response userDetails(Role role) {
+		LOGGER.info("Making request to the {} for the role {}", USER_DETAILS_ENDPOINT, role);
 		return given().spec(RequestSpecWithAuth(role)).when().get(USER_DETAILS_ENDPOINT);
 	}
 }
--- src/test/java/com/demo/Log4JDemo.java
+++ src/test/java/com/demo/Log4JDemo.java
@@ -1,30 +1,24 @@
 package com.demo;
 
-
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log4JDemo {
 	private static Logger logger = LogManager.getLogger(Log4JDemo.class);
+
 	public static void main(String[] args) {
 		logger.info("Inside main method");
 		int a = 10;
-		logger.info("Value of a is {}",a);
+		logger.info("Value of a is {}", a);
 		int b = 0;
-		if(b==0)
-		{
-			logger.warn("Value of b is {}",b);
-		}
-		else
-		{
-			logger.info("Value of b is {}",b);
+		if (b == 0) {
+			logger.warn("Value of b is {}", b);
+		} else {
+			logger.info("Value of b is {}", b);
 		}
 		try {
-		 int result = a/b;
-		}
-		catch(ArithmeticException ex)
-		{
+			int result = a / b;
+		} catch (ArithmeticException ex) {
 			logger.error("Operation cannot happen", ex);
 		}
 		// System.out.println("Result is: " + result);
